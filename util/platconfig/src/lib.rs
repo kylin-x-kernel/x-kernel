8428012c848d957095e0e39212c507fe395ab4b2
yeanwang666 <yeanwang666@126.com>
2026-01-27 10:14:19 +0800

    del apachae

--- util/platconfig/src/lib.rs
+++ util/platconfig/src/lib.rs
@@ -1,6 +1,3 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright (C) 2025-2026 KylinSoft Co., Ltd. https://www.kylinos.cn/
-// See LICENSES for license details.
 //! Platform-specific constants and parameters for X-Kernel.
 //!
 //! Currently supported platform configs can be found in the [configs] directory of
