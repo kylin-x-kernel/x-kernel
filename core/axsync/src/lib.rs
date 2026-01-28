da764303fc6ac3b618f64c1e6732de632e8e52dc
Weikang Guo <guoweikang@kylinos.cn>
2026-01-28 14:16:56 +0800

    remove axsync multitask feature
    Signed-off-by: Weikang Guo <guoweikang@kylinos.cn>

--- core/axsync/src/lib.rs
+++ core/axsync/src/lib.rs
@@ -16,13 +16,6 @@
 
 pub use kspin as spin;
 
-#[cfg(feature = "multitask")]
 mod mutex;
 
-#[cfg(not(feature = "multitask"))]
-#[doc(cfg(not(feature = "multitask")))]
-pub use kspin::{SpinNoIrq as Mutex, SpinNoIrqGuard as MutexGuard};
-
-#[cfg(feature = "multitask")]
-#[doc(cfg(feature = "multitask"))]
 pub use self::mutex::{Mutex, MutexGuard, RawMutex};
