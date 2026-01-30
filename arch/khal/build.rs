46a48d2a37ff10d7f38d39209f078d392d939d8b
huangshijia <1055114578@qq.com>
2026-01-30 10:00:30 +0800

    keep

--- arch/khal/build.rs
+++ arch/khal/build.rs
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 KylinSoft Co., Ltd. <https://www.kylinos.cn/>
+// See LICENSES for license details.
+
 //! Build script for generating the platform linker script.
 
 use std::{io::Result, path::Path};
