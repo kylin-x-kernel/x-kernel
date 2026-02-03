652be60509a5f4c6c2ee6534aa8cf9072636b52b
Weikang Guo <guoweikang@kylinos.cn>
2026-02-03 14:27:10 +0800

    chage rust-toolchain to 1.90
    Signed-off-by: Weikang Guo <guoweikang@kylinos.cn>

--- io/kio/src/lib.rs
+++ io/kio/src/lib.rs
@@ -8,6 +8,7 @@
 #![feature(core_io_borrowed_buf)]
 #![feature(min_specialization)]
 #![feature(maybe_uninit_fill)]
+#![feature(unsigned_signed_diff)]
 #![cfg_attr(not(maybe_uninit_slice), feature(maybe_uninit_slice))]
 
 #[cfg(feature = "alloc")]
