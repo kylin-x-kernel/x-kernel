2c44e215c4be9a00660ae854456b3fa324baed21
luozhixian <luozhixian@kylinos.cn>
2026-02-10 16:12:58 +0800

    fmt code
    Signed-off-by: luozhixian <luozhixian@kylinos.cn>

--- drivers/rs-fdtree/src/kernel_nodes/mod.rs
+++ drivers/rs-fdtree/src/kernel_nodes/mod.rs
@@ -10,7 +10,7 @@ pub mod memory;
 pub mod reserved_memory;
 
 pub use chosen::Chosen;
+pub use dice::Dice;
 pub use interrupt::InterruptController;
 pub use memory::Memory;
 pub use reserved_memory::ReservedMemory;
-pub use dice::Dice;
